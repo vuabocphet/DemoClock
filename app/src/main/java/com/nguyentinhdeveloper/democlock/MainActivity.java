3a3e0d63882aa22e1f46d9a4687fa9983c91f6e9
Developer <vuabocphet98@gmail.com>
2019-07-10 21:49:38 +0700

    Code demo

---
+++ app/src/main/java/com/nguyentinhdeveloper/democlock/MainActivity.java
@@ -0,0 +1,68 @@
+package com.nguyentinhdeveloper.democlock;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import com.andrognito.pinlockview.IndicatorDots;
+import com.andrognito.pinlockview.PinLockListener;
+import com.andrognito.pinlockview.PinLockView;
+
+public class MainActivity extends AppCompatActivity {
+    PinLockView mPinLockView;
+    String TAG = "TAG";
+    private IndicatorDots mIndicatorDots;
+    private static String PASSWORD="1998";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+        mPinLockView = (PinLockView) findViewById(R.id.pin_lock_view);
+        mIndicatorDots = (IndicatorDots) findViewById(R.id.indicator_dots);
+
+        mPinLockView.attachIndicatorDots(mIndicatorDots);
+        mPinLockView.setPinLockListener(mPinLockListener);
+        //mPinLockView.setCustomKeySet(new int[]{2, 3, 1, 5, 9, 6, 7, 0, 8, 4});
+        //mPinLockView.enableLayoutShuffling();
+
+        mPinLockView.setPinLength(6);
+        mPinLockView.setTextColor(ContextCompat.getColor(this, R.color.white));
+
+        mIndicatorDots.setIndicatorType(IndicatorDots.IndicatorType.FILL_WITH_ANIMATION);
+    }
+
+
+
+    private PinLockListener mPinLockListener = new PinLockListener() {
+        @Override
+        public void onComplete(String pin) {
+            Log.d(TAG, "Pin complete: " + pin);
+            if (pin.equals(PASSWORD)){
+                Toast.makeText(MainActivity.this, "Thành công", Toast.LENGTH_SHORT).show();
+            }else {
+                Toast.makeText(MainActivity.this, "Sai", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        @Override
+        public void onEmpty() {
+            Log.d(TAG, "Pin empty");
+        }
+
+        @Override
+        public void onPinChange(int pinLength, String intermediatePin) {
+            Log.d(TAG, "Pin changed, new length " + pinLength + " with intermediate pin " + intermediatePin);
+
+            
+        }
+    };
+}
